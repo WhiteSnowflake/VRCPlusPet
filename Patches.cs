fe3cbb5bdec2e0b90ffa603b7130e8e1ae7cf78f
WhiteSnowflake <82433199+WhiteSnowflake@users.noreply.github.com>
2021-08-09 06:16:27 +0300

    1.2.0 - Updated for new Unity version, replaced FVRCP.

--- Patches.cs
+++ Patches.cs
@@ -1,29 +1,25 @@
 
 using System;
-using System.Linq;
 using System.Reflection;
-using System.Collections.Generic;
 using HarmonyLib;
-using VRC.Core;
 using MelonLoader;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC.Core;
 
 namespace VRCPlusPet
 {
     class Patches
     {
         static HarmonyLib.Harmony modHarmonyInstance = new HarmonyLib.Harmony(BuildInfo.Name);
-        static int
-            lastVRCPMethodNum = 0,
-            maxVRCPPatchNum = 0;
-
-        static PropertyInfo[] vrcpProperties;
-        static Dictionary<PropertyInfo, bool> oldPropertyValues = new Dictionary<PropertyInfo, bool>();
 
         static int
             patchNum = 0,
             patchesCount = 4;
+
+        static GameObject
+            petGoCache,
+            petParentGoCache;
 
         static Transform petTransformCache;
 
@@ -31,11 +27,14 @@ namespace VRCPlusPet
             bubbleImageComponentCache,
             petImageComponentCache;
 
-        static void Patch(MethodBase TargetMethod, HarmonyMethod Prefix, HarmonyMethod Postfix) {
+
+
+        static void Patch(MethodBase TargetMethod, HarmonyMethod Prefix, HarmonyMethod Postfix)
+        {
             try
             {
-                MelonLogger.Msg($"Patching method [{++patchNum}/{patchesCount}] - Success");
                 modHarmonyInstance.Patch(TargetMethod, Prefix, Postfix);
+                MelonLogger.Msg($"Patching method [{++patchNum}/{patchesCount}] - Success");
             }
             catch (Exception e)
             {
@@ -45,87 +44,32 @@ namespace VRCPlusPet
 
         static HarmonyMethod GetLocalPatchMethod(string name) => new HarmonyMethod(typeof(Patches).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
 
-        public static void CheckVRCPPatch()
-        {
-            //Rebuild warning
-            vrcpProperties = typeof(ObjectPublicBoDaBoStApBoStSiDaAcUnique).GetProperties().Where(property => property.Name.Contains("field_Private_Static_Boolean_")).ToArray();
-            maxVRCPPatchNum = vrcpProperties.Count();
-
-            GameObject shortcutMenu = GameObject.Find("UserInterface/QuickMenu/ShortcutMenu");
-            GameObject vrcPlusBanner = GameObject.Find("UserInterface/QuickMenu/ShortcutMenu/HeaderContainer/VRCPlusBanner");
-            GameObject vrcPlusMiniBanner = GameObject.Find("UserInterface/QuickMenu/ShortcutMenu/VRCPlusMiniBanner");
-
-            shortcutMenu.SetActive(true);
-
-            while ((vrcPlusBanner.activeSelf || vrcPlusMiniBanner.activeSelf) && lastVRCPMethodNum < (maxVRCPPatchNum + 1))
-            {
-                shortcutMenu.SetActive(true);
-                shortcutMenu.SetActive(false);
-                shortcutMenu.SetActive(true);
-
-                if (vrcPlusBanner.activeSelf || vrcPlusMiniBanner.activeSelf)
-                {
-                    if (lastVRCPMethodNum >= maxVRCPPatchNum)
-                    {
-                        MelonLogger.Error("Patching method [4] - Error: Proper VRCP method not found, unpatching all methods...");
-
-                        foreach(KeyValuePair<PropertyInfo, bool> keyValuePair in oldPropertyValues)
-                            keyValuePair.Key.SetValue(null, keyValuePair.Value);
-
-                        break;
-                    }
-
-                    PropertyInfo propertyInfo = vrcpProperties[lastVRCPMethodNum++];
-                    oldPropertyValues.Add(propertyInfo, (bool)propertyInfo.GetValue(null));
-                    propertyInfo.SetValue(null, true);
-                }
-                else
-                {
-                    MelonLogger.Warning($"Patching method [4] - Found proper method! VRCP successfuly patched.");
-                    break;
-                }
-            }
-
-            vrcpProperties = null;
-            oldPropertyValues = null;
-            shortcutMenu.SetActive(false);
-        }
-
         public static void DoPatches()
         {
             Utils.LogAsHeader("Patching methods...");
 
             Patch(typeof(VRCPlusThankYou).GetMethod("OnEnable"), GetLocalPatchMethod(nameof(OnEnablePetPatch)), null);
+            Patch(typeof(ShortcutMenu).GetMethod("OnEnable"), null, GetLocalPatchMethod(nameof(OnEnableSMPatch)));
             Patch(typeof(APIUser).GetMethod("get_isSupporter"), null, GetLocalPatchMethod(nameof(FakeVRCPlusSocialPatch)));
             Patch(typeof(APIUser).GetMethod("get_isEarlyAdopter"), null, GetLocalPatchMethod(nameof(FakeVRCPlusSocialPatch)));
 
-            MelonLogger.Msg($"Patching method [4/4] - will be patched after UI init.");
-
-            if (patchNum != (patchesCount - 1))
-            {
-                Utils.LogAsHeader("Patching Failed! Unpatching all methods...");
-                modHarmonyInstance.UnpatchAll();
-            }
-            else
-                Utils.LogAsHeader("Patching complete!");
+            Utils.LogAsHeader("Patching complete!");
         }
 
         #region Patches
         static void FakeVRCPlusSocialPatch(APIUser __instance, ref bool __result)
         {
-            if (__instance.IsSelf && Utils.GetPref(VRCPlusPet.mlCfgNameFakeVRCP))
+            if (__instance.IsSelf && Utils.GetPref(VRCPlusPet.mlCfgFakeSocialVRCP))
                 __result = true;
         }
 
-        static bool FakeVRCPlusPatch(ref bool __result)
+        static void OnEnableSMPatch(VRCPlusThankYou __instance)
         {
-            if (vrcpProperties != null || Utils.GetPref(VRCPlusPet.mlCfgNameFakeVRCP))
+            if (petGoCache != null && Utils.GetPref(VRCPlusPet.mlCfgForceEnablePet))
             {
-                __result = true;
-                return false;
+                petGoCache.SetActive(true);
+                petParentGoCache.SetActive(true);
             }
-
-            return true;
         }
 
         static void OnEnablePetPatch(VRCPlusThankYou __instance)
@@ -142,7 +86,11 @@ namespace VRCPlusPet
                 __instance.field_Public_List_1_AudioClip_0 = Utils.audioClips; //sounds
 
             if (petTransformCache == null)
+            {
                 petTransformCache = __instance.transform;
+                petGoCache = petTransformCache.gameObject;
+                petParentGoCache = petTransformCache.parent.gameObject;
+            }
 
             if (VRCPlusPet.bubbleSprite != null)
             {
